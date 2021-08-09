212f0bfa2ce76d63d17d35b2443aa266437bfc2f
rokaskvasas <rokas.kvasas@gmail.com>
2021-08-09 14:41:48 +0300

    v0.1.3 struggle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,23 +19,39 @@ public class App {
             e.printStackTrace();
         }
 //        ArrayList<Product> sortedArray = allproducts.stream().sorted(Comparator.comparing(Product::getId)).collect(Collectors.toCollection(ArrayList::new));
-catalogs(allproducts);
+        catalogs(allproducts);
 
         ArrayList<Product> ss = allproducts.stream().filter(product -> product.getParentId().equals("Null")).collect(Collectors.toCollection(ArrayList::new));
+        System.out.println("test");
     }
-    public static HashMap<String, Pair<String,String>> catalogs(ArrayList<Product> array){
-        HashMap<String, Pair<String,String>> catalog = new HashMap<>();
+    public static HashMap<List<Product>, Pair<List<Product>,List<Product>>> catalogs(ArrayList<Product> array){
+        HashMap<List<Product>, Pair<List<Product>,List<Product>>> catalog = new HashMap<>();
         List<Product> First = array.stream().filter(product -> product.getParentId().equals("Null")).collect(Collectors.toList());
-        List<Product> buitine = array.stream().filter(product -> product.getParentId().equals("4")).collect(Collectors.toList());
-        List<Product> indaploves = array.stream().filter(product -> product.getParentId().equals("33")).collect(Collectors.toList());
+        List<Product> buitinetechnika = array.stream().filter(product -> product.getParentId().equals("4")).collect(Collectors.toList());
         List<Product> tv = array.stream().filter(product -> product.getParentId().equals("2")).collect(Collectors.toList());
         List<Product> phones = array.stream().filter(product -> product.getParentId().equals("3")).collect(Collectors.toList());
+        List<Product> sveikatairgrozis = array.stream().filter(product -> product.getParentId().equals("1")).collect(Collectors.toList());
+        List<Product> indaploves2 = array.stream().filter(product -> product.getParentId().equals("33")).collect(Collectors.toList());
+        List<Product> saldytuvai = array.stream().filter(product -> product.getParentId().equals("31")).collect(Collectors.toList());
+        List<Product> garuSurinktuvai = array.stream().filter(product -> product.getParentId().equals("34")).collect(Collectors.toList());
+        List<Product> kavosAparatai = array.stream().filter(product -> product.getParentId().equals("10")).collect(Collectors.toList());
+        List<Product> ledEkranai = array.stream().filter(product -> product.getParentId().equals("6")).collect(Collectors.toList());
+        List<Product> antenos = array.stream().filter(product -> product.getParentId().equals("32")).collect(Collectors.toList());
+        List<Product> smartphones = array.stream().filter(product -> product.getParentId().equals("5")).collect(Collectors.toList());
+        List<Product> phoneDevices = array.stream().filter(product -> product.getParentId().equals("8")).collect(Collectors.toList());
+        List<Product> telefonuApratai = array.stream().filter(product -> product.getParentId().equals("7")).collect(Collectors.toList());
+        List<Product> fenai = array.stream().filter(product -> product.getParentId().equals("9")).collect(Collectors.toList());
+        Pair<List<Product>,List<Product>> indaploves1 =Pair.of(buitinetechnika,indaploves2);
+        catalog.put(First,indaploves1);
+
+
 
 
 //        catalog.forEach(First.stream().filter(product -> product.getName().equals("Buitine technika")),buitine.forEach(Product::getName),indaploves.forEach(Product::getName));
 //
 //
+        return catalog;}
 
-        return null;}
-
+//    public static List<Product> arrayfactory(ArrayList<Product> array,String arrName, String identify){
+//    return List<Product> arrName= array.stream().filter(product -> product.getParentId().equals(identify)).collect(Collectors.toList()); }
 }
