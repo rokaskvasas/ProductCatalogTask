98c498e31380c217abbbc89fad3319b96ae091ee
rokaskvasas <rokas.kvasas@gmail.com>
2021-08-09 17:15:43 +0300

    v0.3.0 struggle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -32,40 +32,68 @@ public class App {
 
     }
     public static HashMap<List<Product>, Pair<List<Product>,List<Product>>> catalogs(ArrayList<Product> array){
-        HashMap<List<Product>, Pair<List<Product>,List<Product>>> catalog = new HashMap<>();
+        HashMap<List<Product>, Pair<List<Product>,List<Product>>> catalog = new HashMap<>(); // pakeisti
+
+        // filtering by section
         List<Product> First = filterSection(array,"Null");
         List<Product> buitinetechnika = filterSection(array,"4");
         List<Product> tv = filterSection(array,"2");
         List<Product> phones = filterSection(array,"3");
         List<Product> sveikatairgrozis = filterSection(array,"1");
-        List<Product> indaploves2 = filterSection(array,"33");
-        List<Product> saldytuvai = filterSection(array,"31");
-        List<Product> garuSurinktuvai = filterSection(array,"34");
-        List<Product> kavosAparatai = filterSection(array,"10");
-        List<Product> ledEkranai = filterSection(array,"6");
-        List<Product> antenos = filterSection(array,"32");
-        List<Product> smartphones = filterSection(array,"5");
-        List<Product> phoneDevices = filterSection(array,"8");
-        List<Product> telefonuApratai = filterSection(array,"7");
+        List<Product> indaploves3 = filterSection(array,"33");
+        List<Product> saldytuvai3 = filterSection(array,"31");
+        List<Product> garuSurinktuvai3 = filterSection(array,"34");
+        List<Product> kavosAparatai3 = filterSection(array,"10");
+        List<Product> ledEkranai3 = filterSection(array,"6");
+        List<Product> antenos3 = filterSection(array,"32");
+        List<Product> smartphones3 = filterSection(array,"5");
+        List<Product> telefonuaparatai3 = filterSection(array,"8");
+        List<Product> telefonupriedai3 = filterSection(array,"7");
         List<Product> fenai = filterSection(array,"9");
-        Pair<List<Product>,List<Product>> indaploves1 =groupingSection(buitinetechnika,indaploves2);
-//        Pair<List<Product>,List<Product>>
-        HashMap<String,List<Product>> indaploves3 = new HashMap<>();
-        indaploves3.put(parentIdToString(buitinetechnika,33),indaploves2);
+
+        // grouping together second and third section.
+        HashMap<String,List<Product>> indaploves2 = new HashMap<>();
+        indaploves2.put(parentIdToString(buitinetechnika,33),indaploves3);
+        HashMap<String,List<Product>> saldytuvai2 = new HashMap<>();
+        saldytuvai2.put(parentIdToString(buitinetechnika,31),saldytuvai3);
+        HashMap<String,List<Product>> garuSurinktuvai2 = new HashMap<>();
+        garuSurinktuvai2.put(parentIdToString(buitinetechnika,34),garuSurinktuvai3);
+        HashMap<String,List<Product>> kavosAparatai2 = new HashMap<>();
+        kavosAparatai2.put(parentIdToString(buitinetechnika,10),kavosAparatai3);
+        HashMap<String,List<Product>> ledEkranai2 = new HashMap<>();
+        ledEkranai2.put(parentIdToString(tv,6),ledEkranai3);
+        HashMap<String,List<Product>> antenos2 = new HashMap<>();
+        antenos2.put(parentIdToString(tv,32),antenos3);
+        HashMap<String,List<Product>> smartphones2 = new HashMap<>();
+        smartphones2.put(parentIdToString(phones,5),smartphones3);
+        HashMap<String,List<Product>> telefonuaparatai2 = new HashMap<>();
+        telefonuaparatai2.put(parentIdToString(phones,8),telefonuaparatai3);
+        HashMap<String,List<Product>> telefonupriedai2 = new HashMap<>();
+        telefonupriedai2.put(parentIdToString(phones,7),telefonupriedai3);
+        HashMap<String,List<Product>> sveikatairgrozis2 = new HashMap<>();
+        sveikatairgrozis2.put(parentIdToString(sveikatairgrozis,9),fenai);
 
 
-        catalog.put(First,indaploves1);
+
+
+
+
+
+
+
+
+
 
         return catalog;}
 
 public static List<Product> filterSection(ArrayList<Product> array,String parentID){
-        return array.stream().filter(product -> product.getParentId().equals(parentID)).collect(Collectors.toList());
-}
-public static Pair<List<Product>,List<Product>> groupingSection(List<Product> first,List<Product> second){
-        return Pair.of(first, second);
-}
-public static String parentIdToString(List<Product> list1,int id){
-        return list1.stream().filter(product -> product.getId()==id).map(Product::getName).findFirst().toString();
-}
+        return array.stream().filter(product -> product.getParentId().equals(parentID)).collect(Collectors.toList());}
 
+public static String parentIdToString(List<Product> list1,int id){
+        return list1.stream().filter(product -> product.getId()==id).map(Product::getName).findFirst().toString();}
+
+//public static HashMap<String,List<Product>> groupBySection(List<Product> productNameInList,int productID,List<Product> sectionProducts,String name){
+//    HashMap<String,List<Product>> name =new HashMap<>()
+//        return
+//}
 }
